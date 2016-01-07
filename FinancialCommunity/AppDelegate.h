2ea5bd24e8c89e503577ec3d8905d6aa64aacd2a
iOSCaiNiao <caozcisdemo@163.com>
2016-01-07 23:47:42 +0800

    Initial Commit

---
+++ FinancialCommunity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FinancialCommunity
+//
+//  Created by 中超 曹 on 16/1/7.
+//  Copyright (c) 2016年 Czc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FinancialCommunity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FinancialCommunity
+//
+//  Created by 中超 曹 on 16/1/7.
+//  Copyright (c) 2016年 Czc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
